a81273dffed0edcc052e0def8ebb27c128485c16
miaardi <ardissonemia@gmail.com>
2025-09-19 12:46:09 -0300

    terminada

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,19 +2,7 @@
 # Aqui deberas implementar la clase Camion y resolver los puntos a, b, c, d y f segun las consignas
 
 
-class Camion:
-    patentes_usadas = set() #creo esta variable para poder identificar si esa patente fue usada onno
-    def __init__(self, patente, marca, carga, anio):
-        if patente in Camion.patentes_usadas: #raiseo un error si la patente ya fue usada
-            raise ValueError(f"La patente {patente} ya está registrada.")
-        self.patente = patente
-        self.marca = marca
-        self.carga = carga
-        self.anio = anio
-        Camion.patentes_usadas.add(patente) #agrego la patente al registro
 
-    def __str__(self):
-        return f"Camión: #{self.patente} \nCarga: {self.carga} \nMarca: {self.marca} \nAnioo: {self.anio}"
     
     #def __eq__(self, other): ###B
         #if not isinstance(other, Camion): #si el objeto a comparar es de la misma clase que camion va al return ()
@@ -23,11 +11,212 @@ class Camion:
                # self.marca == other.marca and
                # self.carga == other.carga and
                # self.anio == other.anio)
+class Camion:
+    patentes_usadas = {} #creo esta variable para poder identificar si esa patente fue usada onno
+    marcas_disp=['volskwagen','fiat','bmw']
+    def __init__(self, patente, marca, carga, anio):
+        self.validar_patente(patente)
+        self.validar_marca(marca)
+        self.carga= self.validar_int(carga)
+        self.anio = self.validar_int(anio)
+        
+         
+    def __str__(self):
+        return f"Camión: #{self.patente} \nCarga: {self.carga} \nMarca: {self.marca} \nAnio: {self.anio}"
     
+    def validar_str(self,cadena):
+        if not isinstance(cadena,str):
+             raise TypeError('Debe ser un string')
+        if len(cadena)==0:
+            raise ValueError('La cadena no debe estar vacia')
+        else:
+            return cadena
+        
+    def validar_patente(self,patente):
+        patente= self.validar_str(patente)
+        patente=patente.strip().upper()
+        if len(patente)!=7:
+            raise ValueError('La patente debe estar conformada por dos letras, tres numeros y luego dos letras')
+        if patente in Camion.patentes_usadas: #raiseo un error si la patente ya fue usada
+                raise ValueError(f"La patente {patente} ya está registrada.")
+        else:
+            self.patente=patente
+            #despues de crear la instancia, uso registrar patente
+    
+    def validar_marca(self,marca):
+        marca=self.validar_str(marca)
+        marca=marca.lower()
+        if marca not in Camion.marcas_disp:
+            raise ValueError('La marca no esta dentro de las disponibles')
+        else:
+            self.marca=marca
+       
+    def validar_int(self,numero):   
+        if not isinstance(numero,int):
+            raise TypeError('Debe ser un numero')
+        elif numero is None:
+            raise ValueError('No debe estar vacia')
+        else:
+            return numero
+            
+  
     def __eq__(self, other): ###c
         if not isinstance(other, Camion): #si el objeto a comparar es de la misma clase que camion va al return ()
             return False #si no son de la misma clase ni se gasta porque es al pepe
         return (self.patente == other.patente) #igual que el b solo que solo me compara la patente
+    
+    def get_patente(self):
+        return self.patente
+    
+    def get_marca(self):
+        return self.marca
+    
+    def get_carga(self):
+        return self.carga
+    
+    def get_anio(self):
+        return self.anio
+    
+    def set_carga(self, nueva_carga):
+        nueva_carga=self.validar_int(Camion,nueva_carga)
+        
+    
+def modificar_carga(patente,nueva_carga):
+    patente= Camion.validar_str(Camion,patente)
+    patente=patente.strip().upper()
+    if len(patente)!=7:
+        raise ValueError('La patente debe estar conformada por dos letras, tres numeros y luego dos letras')
+    if patente in Camion.patentes_usadas:
+        Camion.set_carga(Camion,nueva_carga)
+        Camion.patentes_usadas[patente].carga=nueva_carga
+        print('Se ha cambiado la carga con exito')
+              
+    else:
+        raise ValueError('La patente ingresada no se encuentra registrada')  
+        
+def render_camiones():
+    camiones_ordenados=(sorted(Camion.patentes_usadas.values(),key=lambda obj: obj.anio)) #agarra cada objeto y se fija el anio
+    for obj in camiones_ordenados:
+        print(f"{obj.patente} {obj.marca} {obj.carga} {obj.anio}")
+              
+
+
+def max_marca():
+    conteo = {}
+    for obj in Camion.patentes_usadas.values():
+        marca = obj.marca #mira cada marca de los objetos
+        conteo[marca] = conteo.get(marca, 0) + 1
+    tupla_max=max(conteo.items(), key=lambda x: x[1]) #x es una de las tuplas del dict conteo, x[1] es la posicion donde esta al cantidad
+    print(f'La marca mas utilizada es: {tupla_max[0]}')
+    
+
+def menu(Camiones):
+    while True:
+        try:
+            numero=int(input('1 Registrar un nuevo camion \n2 Modificar la carga de un camion \n3 Mostrar la lista de camiones registrados \n4 Mostrar la marca que mas fue registrada \n5 Cerrar el programa \n Ingrese el numero deseado: '))
+        except ValueError:
+            print('Debe ingresar un numero dentro de los mostrados')
+            continue
+        if numero ==1:
+            try:
+                patente=str(input('Ingrese la patente: '))
+                marca=str(input('Ingrese la marca: '))
+                carga=int(input('Ingrese la carga: '))
+                anio=int(input('Ingrese el anio: '))
+                Ingreso=Camion(patente,marca,carga,anio)
+                Camiones.append(Ingreso)
+            
+            except ValueError:
+                print('Ingrese datos correctos')
+            continue
+        elif numero==2:
+            try:
+                patente_carga = input("Ingrese la patente del camión a editar la carga: ")
+                encontrado = False
+                for camion in Camiones:
+                    if camion.patente == patente_carga:
+                        nuevo_valor = int(input("Ingrese la nueva carga: "))
+                        camion.carga = nuevo_valor #aca en realidad deberia usar un setter
+                        encontrado = True
+                        print("Carga actualizada correctamente")
+                    break
+                if not encontrado:
+                    print("La patente no está cargada, registre un nuevo camión")
+                else:
+                    print("La patente no está registrada, primero registre el camión")    
+            except ValueError:
+                print('Los valores ingresados no son correctos')
+            continue
+            
+        elif numero==3:
+            ordenados = sorted(Camiones, key=lambda c: c.anio)
+            print(ordenados)
+        elif numero==4:
+                #la logica es que pase por toda la lista y que vaya contando cuantas son = (agarra 1, compara y agrega al contador, y dsp pasa a la siguiente que es distinta a la primera y asi hasta termianr.)
+            conteo = {}
+            for c in Camiones:
+                if c.marca in conteo:
+                    conteo[c.marca] += 1
+                else:
+                    conteo[c.marca] = 1
+            max_marca = max(conteo, key=conteo.get)
+            print(f'La marca mas registrada es: {max_marca}')
+            continue
+        elif numero==5:
+            print('Chau')
+            break
+        else:
+            print('Debe ingresar un numero dentro de los mostrados')
+            continue
+
+
+
+if __name__ == "__main__":
+    #registar camiones
+    try:
+        c1=Camion('ab131cd','fiat',500 ,2003)
+        Camion.patentes_usadas[c1.patente]=c1
+        print('Camion c1 creado con exito')
+        
+        c2=Camion('aa131cd','Bmw',300, 2005)
+        Camion.patentes_usadas[c2.patente]=c2
+        print('Camion c2 creado con exito')
+        
+        c3=Camion('as131cd','volskwagen',100 ,1999)
+        Camion.patentes_usadas[c3.patente]=c3
+        print('Camion c3 creado con exito')
+        
+        c4=Camion('aa131dd','Bmw',5, 2025)
+        Camion.patentes_usadas[c4.patente]=c4
+        print('Camion c4 creado con exito')
+        
+        
+    except TypeError as e:
+        print('El error es: ', e)
+        '''sacar todos los errores e imprimirlos, y dsp exception'''
+    except ValueError as e:
+        print('El error es: ', e)
+    except Exception as e:
+        print('El error es: ', e)
+        
+    #modificar carga
+    try:
+        patente='ab131cd'
+        nueva_carga= 22
+        modificar_carga(patente,nueva_carga)
+        print(c1)
+    except ValueError as e:
+        print('El error es: ', e)   
+    except ValueError as e:
+        print('El error es: ', e)
+    except Exception as e:
+        print('El error es: ', e)     
+    #camiones registrados
+    render_camiones()
+    #max marca
+    max_marca()
+      
+    
     
     
 # a. Indica que devuelven las siguientes expresiones. Analizalo con tus companeros y luego ejecuta las instrucciones en la maquina para comprobar tu respuesta.
@@ -87,7 +276,7 @@ def menu(Camiones):
                 for camion in Camiones:
                     if camion.patente == patente_carga:
                         nuevo_valor = int(input("Ingrese la nueva carga: "))
-                        camion.carga = nuevo_valor
+                        camion.carga = nuevo_valor #aca en realidad deberia usar un setter
                         encontrado = True
                         print("Carga actualizada correctamente")
                     break
@@ -121,7 +310,4 @@ def menu(Camiones):
             continue
 
 
-if __name__ == "__main__":
-    Camiones=[]
-    menu(Camiones)
-        
+
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,12 +1,152 @@
 # Ejercicio 2: Modelar una computadora
 # 
-# En este archivo debés crear la clase Computadora siguiendo las consignas del README.
+# En este archivo debes crear la clase Computadora siguiendo las consignas del README.
 # Recordá:
 # - Definir atributos relevantes en el constructor (__init__), con valores por defecto.
-# - Implementar el método __str__ para mostrar la información esencial.
+# - Implementar el método __str__ para mostrar la informacion esencial.
 # - Instanciar al menos 3 computadoras con distintos valores.
 # - Llevar la cuenta de computadoras creadas (usar variable de clase).
-# - Implementar al menos 2 métodos de los sugeridos (updateOS, PM, addRAM, getCapacity).
-# - Crear otra clase para un componente (ej: Disco, RAM, etc.) con su propio __init__, __str__ y al menos un método.
+# - Implementar al menos 2 metodos de los sugeridos (updateOS, PM, addRAM, getCapacity).
+# - Crear otra clase para un componente (ej: Disco, RAM, etc.) con su propio __init__, __str__ y al menos un metodo.
 # 
-# ¡No olvides probar todos los métodos y comentar tu criterio para los valores
+# ¡No olvides probar todos los metodos y comentar tu criterio para los valores
+
+class Computadora():
+    lista_compus=[]
+    modelos_disp=['air','pro']
+    marcas_disp=['mac','samsung']
+    
+    def __init__(self, id:str, modelo:str, marca:str):
+        self.validar_id(id)
+        self.set_modelo(modelo)
+        self.set_marca(marca)
+    
+        
+    def __str__(self):
+        return f'La computadora de id: {self.id} es de modelo: {self.modelo} y marca: {self.marca}'
+    '''visualizo todos los atributos de un objeto'''
+    
+    #repr para cuando no quiero imprimir en cad ainstancia sino la lista completa d eobjetos y esta en *lista, dict etc)
+    
+    def __eq__(self,other):
+        if not isinstance(other, Computadora):
+            return NotImplemented
+        return self.id==other.id
+    '''estblaezco que el id debe ser unico para cada computadora entonces cuando quiera comparar me compara el valor'''
+    
+    def validar_cadena(self, cadena: str):
+        if not isinstance(cadena,str):
+            raise TypeError('Debe ser una cadena')
+        if len(cadena)==0:
+            raise ValueError('No debe estar vacio')
+    
+    def get_id(self):
+        return self.id
+    
+    def get_marca(self):
+        return self.marca
+    
+    def get_modelo(self):
+        return self.modelo
+    
+    def validar_id(self,id):
+        self.validar_cadena(id) #asi cada vez que cambio el id me valida que sea sr y no este vacio
+        id=id.lower()
+        if id in Computadora.lista_compus:
+            raise ValueError('Ese id ya esta ingresado')
+        self.id=id
+        Computadora.lista_compus.append(id)
+    
+    def set_id(self, nuevo_id:str):
+        self.validar_cadena(nuevo_id) #asi cada vez que cambio el id me valida que sea sr y no este vacio
+        nuevo_id=nuevo_id.lower()
+        if nuevo_id in Computadora.lista_compus:
+            raise ValueError('Ese id ya esta ingresado')
+        Computadora.lista_compus.remove(self.id)
+        self.id=nuevo_id
+        Computadora.lista_compus.append(nuevo_id)
+        
+    def set_modelo(self, nuevo_modelo:str):
+        nuevo_modelo=nuevo_modelo.lower()
+        self.validar_cadena(nuevo_modelo)
+        if nuevo_modelo not in Computadora.modelos_disp:
+            raise ValueError('Ese modelo no se encuentra dentro de los disponibles') 
+        self.modelo=nuevo_modelo
+        
+    def set_marca(self, nueva_marca:str):
+        nueva_marca=nueva_marca.lower()
+        self.validar_cadena(nueva_marca)
+        if nueva_marca not in Computadora.marcas_disp:
+            raise ValueError('Esa marca no se encuentra dentro de las disponibles') 
+        self.marca=nueva_marca    
+            
+            
+    #funcionales, mi idea es, si el id esta en lista compus entonces la puedo vender, sino no.
+    
+def vender(id):
+    if id not in Computadora.lista_compus:
+        raise ValueError('El id de esa computadora ya fue vendido')
+    Computadora.lista_compus.remove(id)
+    print('Venta completada con exito') 
+    
+def mostrar_lista_compus():
+    if not Computadora.lista_compus:
+        for i,v in enumerate(Computadora.lista_compus,1):
+            print(i,v)
+    else:
+        print('No hay compus para vender')
+    
+def menu():
+    numeros=int(input('Para cara ver la lista de computadoras disponibles escribe 1\n Para comprar una computadora escribe 2\n Para salir del menu escribe 3'))
+    while numeros!=3:
+        if numeros==1:
+            mostrar_lista_compus()
+            numeros=int(input('Para cara ver la lista de computadoras disponibles escribe 1\n Para comprar una computadora escribe 2\n Para salir del menu escribe 3'))
+        if numeros==2:
+            try:
+                id_comprar=str(input('Ingrese el id de la computadora a comprar: '))
+                if id_comprar not in Computadora.lista_compus:
+                    print('El id de esa computadora ya fue vendido')
+                Computadora.lista_compus.remove(id_comprar)
+                print('Venta completada con exito')
+            except ValueError as e:
+                print('Hubo un error')
+                numeros=int(input('Para cara ver la lista de computadoras disponibles escribe 1\n Para comprar una computadora escribe 2\n Para salir del menu escribe 3'))
+            
+                
+        
+if __name__=='__main__':
+    '''para que si creo herencias, que solo me corra esto si es esta clase'''
+    try:
+        c1=Computadora('A2','pro','mac')
+        print(c1)
+        c2=Computadora('A3','air','samsung')
+        print(c2)
+        c3=Computadora('A4','air','mac')
+        print(c3)
+        #c4=Computadora(3,'pro','mac')
+        #print(c4)
+        #c5=Computadora('A3','aires','samsung')
+        #print(c5)
+        c4=Computadora('ee','air','4')
+        print(c4)
+        '''siempre imprimir todo par chequear que me devuelve'''
+    except TypeError as e:
+        print('El error es 1: ', e)
+        '''sacar todos los errores e imprimirlos, y dsp exception'''
+    except ValueError as e:
+        print('El error es 2: ', e)
+    except Exception as e:
+        print('El error es 3: ', e)
+    #vender compu
+    
+    if 'A2'.lower() not in Computadora.lista_compus:
+        print('El id de esa computadora no existe')
+    else: 
+        Computadora.lista_compus.remove('A2'.lower())
+        print('Venta completada con exito')
+
+    
+    
+    
+    
